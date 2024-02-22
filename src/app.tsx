30a90ab4a8ff777edfe73ac79aef52252eeae28e
Thais Megumi <tha.megumi@gmail.com>
2024-02-22 07:15:03 -0300

    feat: create pagination

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { FileDown, MoreHorizontal, Plus, Search } from "lucide-react";
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
 import { Header } from "./components/header";
 import { Tabs } from "./components/tabs";
 import { Button } from "./components/ui/button";
@@ -12,8 +12,64 @@ import {
   TableRow,
 } from "./components/ui/table";
 import { Pagination } from "./components/pagination";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useDebounceValue from "./hooks/use-debounce-value";
+
+export interface TagResponse {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+}
+
+export interface Tag {
+  title: string;
+  amountOfVideos: number;
+  id: string;
+}
 
 export function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const urlFilter = searchParams.get("filter") ?? "";
+  const [filter, setFilter] = useState<string>(urlFilter);
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ["get-tags", urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`
+      );
+      const data = await response.json();
+
+      //Delay 2s
+      // await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      return data;
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  function handleFilter() {
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", String(filter));
+
+      return params;
+    });
+  }
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -31,8 +87,17 @@ export function App() {
 
         <div className="flex items-center justify-between">
           <Input variant="filter">
-            <Search className="size-3" />
-            <Control placeholder="Search tags..." />
+            <div className="flex flex-items">
+              <Search className="size-3" />
+              <Control
+                placeholder="Search tags..."
+                onChange={(e) => setFilter(e.target.value)}
+              />
+              <Button onClick={handleFilter}>
+                <Filter className="size-3" />
+                Filter
+              </Button>
+            </div>
           </Input>
 
           <Button>
@@ -51,16 +116,18 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => (
-              <TableRow key={index}>
+            {tagsResponse?.data.map((tag) => (
+              <TableRow key={tag.id}>
                 <TableCell></TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-0.5">
-                    <span className="font-medium">React</span>
-                    <span className="text-xs text-zinc-500">uuid</span>
+                    <span className="font-medium">{tag.title}</span>
+                    <span className="text-xs text-zinc-500">{tag.id}</span>
                   </div>
                 </TableCell>
-                <TableCell className="text-zinc-300">13 video(s)</TableCell>
+                <TableCell className="text-zinc-300">
+                  {tag.amountOfVideos} video(s)
+                </TableCell>
                 <TableCell className="text-right">
                   <Button size="icon">
                     <MoreHorizontal className="size-4" />
@@ -71,7 +138,13 @@ export function App() {
           </TableBody>
         </Table>
 
-        <Pagination pages={1} items={10} page={0} />
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        )}
       </main>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
